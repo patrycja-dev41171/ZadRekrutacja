205f42afedad3ca2eaa72f9f9967873f74ee4351
Patrycja Ińska <patrycja.inska@interia.pl>
2022-12-01 19:01:35 +0100

    Sass implementation. I created a style folder and in it the main Sass styles file and the _variables file with variables (colors, fonts).Sass implementation. I created a style folder and in it the main Sass styles file and the _variables file with variables (colors, fonts). I did a style reset in the index.scss file.

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.scss";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-      <BrowserRouter>
-          <App />
-      </BrowserRouter>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
+
 reportWebVitals();
