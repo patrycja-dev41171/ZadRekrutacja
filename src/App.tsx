fcc645378c822d3a5f17a69bc388724a9dcea965
Patrycja Ińska <patrycja.inska@interia.pl>
2022-12-01 18:23:30 +0100

    I created folders and files for app views. I implemented routing in the App.tsx file.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./components/views/Home/Home";
+import { Register } from "./components/views/Register/Register";
 
 function App() {
-  return <></>;
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/register" element={<Register />} />
+    </Routes>
+  );
 }
 
 export default App;
