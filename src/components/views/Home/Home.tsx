580b1843cc92bf77bc112ac5c62ceaff3e566ed8
Patrycja Ińska <patrycja.inska@interia.pl>
2022-12-02 16:57:23 +0100

    Create navigation for the Home view. Creating a style for the p element (name of task author).

--- src/components/views/Home/Home.tsx
+++ src/components/views/Home/Home.tsx
@@ -1,5 +1,17 @@
 import React from "react";
+import { Container } from "../../common/Container/Container";
+import "./Home.scss";
+import { Button } from "../../common/Button/Button";
 
 export const Home = () => {
-  return <>Home</>;
+  return (
+    <>
+      <Container>
+        <nav className="home__nav">
+          <p>Patrycja Ińska</p>
+          <Button title="formularz rejestracyjny" color="gray" size="big" />
+        </nav>
+      </Container>
+    </>
+  );
 };
