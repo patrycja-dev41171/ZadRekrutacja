9c236c39d63fb9debc87082e6216d75dafe8c382
Patrycja Ińska <patrycja.inska@interia.pl>
2022-12-03 23:36:04 +0100

    Implementation of the header structure and styles for the register view.

--- src/components/views/Register/Register.tsx
+++ src/components/views/Register/Register.tsx
@@ -1,5 +1,14 @@
 import React from "react";
+import { Container } from "../../common/Container/Container";
+import './Register.scss';
 
 export const Register = () => {
-  return <>Register</>;
+  return (
+    <Container>
+      <header className="register__header">
+          <h1 className="register__header__h1">formularz rejestracyjny</h1>
+          <div className="register__header__rectangle"></div>
+      </header>
+    </Container>
+  );
 };
